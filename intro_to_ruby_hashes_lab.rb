febd74b0ef37b3b10ccae7c098cf35acc9473164
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-30 20:41:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,11 @@ def monopoly_with_third_tier = {
    :pieces => 4
 },
 "rent_in_dollars"
- :
- :
+ :one_piece_owned=>
+ :two_poieces_owned=>
+ :three_pieces_owned=>
+ :four_piences_owned=>
+}
 
 
   # When you start writing the implementation for this method, copy the Hash
