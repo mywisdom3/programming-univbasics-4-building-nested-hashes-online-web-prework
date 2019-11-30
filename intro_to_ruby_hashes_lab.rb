1057e490e5ab5bb1002840c6d91c468199362348
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-30 20:50:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,12 +35,8 @@ def monopoly_with_third_tier = {
  :pennsylvania_railroad{}
  :b_and_o_railroad {}
  :shortline_railroad {}
- 
- 
- 
- 
- 
-  # When you start writing the implementation for this method, copy the Hash
+}
+   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
